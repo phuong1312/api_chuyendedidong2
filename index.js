86a0b3bb1a27e733379618bf687ca3b2c3ed2bb5
MinhThang <20211tt1477@mail.tdc.edu.vn>
2022-12-17 14:33:16 +0700

    up

--- index.js
+++ index.js
@@ -3,29 +3,46 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const morgan = require("morgan");
+
 require("dotenv").config({ path: __dirname + "/.env" });
 
 const port = process.env.PORT;
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const socketIo = require("socket.io")(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("common"));
 app.use(cors());
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(morgan('common'));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(morgan("common"));
 
 // app.set("view engine", "ejs");
 //connect
 var dbConn = require("./config/config");
 mongoose.connection.once("open", () => {
   console.log("Connect mongodb");
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log("server api port " + port);
     setInterval(function () {
       console.log("server api port " + port);
     }, 300000);
+  });
+});
+
+socketIo.on("connection", (socket) => {
+  console.log("có người đang kết nối: " + socket.id);
+
+  socket.on("client-up-color", (data) => {
+    console.log("Sever nhận color từ client: " + data);
+    socketIo.sockets.emit("sever-up-color", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected: ");
   });
 });
 
@@ -37,7 +54,7 @@ const areaRoute = require("./routes/area");
 const tableRoute = require("./routes/table");
 const drinkOrderRoute = require("./routes/drinkOrder");
 const orderRoute = require("./routes/order");
-const authorization = require('./config/authTokenRequired');
+const authorization = require("./config/authTokenRequired");
 
 app.use("/api/user", userRoute);
 app.use("/api/role", roleRoute);
@@ -48,8 +65,8 @@ app.use("/api/table", tableRoute);
 app.use("/api/drinkorder", drinkOrderRoute);
 app.use("/api/order", orderRoute);
 
-app.get('/', authorization, (req, res) => {
-    console.log(req.user);
-    return res.json(req.user);
+app.get("/", authorization, (req, res) => {
+  console.log(req.user);
+  return res.json(req.user);
 });
 module.exports = app;
